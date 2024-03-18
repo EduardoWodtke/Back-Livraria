514b557c7a3e8253442c5e32b41c334321811a2b
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-03-18 11:04:38 -0300

    Formatação com Isort e Black

--- core/serializers/editora.py
+++ core/serializers/editora.py
@@ -1,11 +1,11 @@
-# Theodoro podre! 
+# Theodoro podre!
 
 from rest_framework.serializers import ModelSerializer
 
 from core.models import Editora
 
+
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
-        fields = '__all__'
-
+        fields = "__all__"
