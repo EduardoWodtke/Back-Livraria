5e15955f61d1c5d1dda93f85130b57d636a4c12b
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-07-29 11:36:59 -0300

    Adição do parametro autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -10,7 +10,8 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=True, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=False, blank=False)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="autores")
 
 
     def __str__(self):
-        return f"(id {self.id}) {self.titulo} R${self.preco} | informações adicionais: Categoria: {self.categoria}, Editora: {self.editora}, (Qnt:{self.quantidade})|"
+        return f"(id {self.id}) {self.titulo} R${self.preco} | informações adicionais: Categoria: {self.categoria}, Editora: {self.editora}, Autor:{self.autor}, (Qnt:{self.quantidade})|"
