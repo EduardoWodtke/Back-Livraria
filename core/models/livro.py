7fa1e466f176c70289dc27346fe6d7b39598ad46
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-07-29 11:30:12 -0300

    Criação da tabela Livro

---
+++ core/models/livro.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+from core.models import Autor, Categoria, Editora
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=255, blank=True, null=True)
+    quantidade = models.IntegerField(default=0, blank=True, null=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=False, blank=False)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+
+
+    def __str__(self):
+        return f"(id {self.id}) {self.titulo} R${self.preco} | informações adicionais: Categoria: {self.categoria}, Editora: {self.editora}, (Qnt:{self.quantidade})|"
