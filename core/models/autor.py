c34cc4f92f91da669b155568d4da406ae6cf24d8
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-08-23 14:58:28 -0300

    Adicionando mais elementos no banco de dados e corrigindo erros ortografios.

--- core/models/autor.py
+++ core/models/autor.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome.upper()} ({self.id})"
--- core/models/livro.py
+++ core/models/livro.py
@@ -9,9 +9,9 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=255, blank=True, null=True)
     quantidade = models.IntegerField(default=0, blank=True, null=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=True, null=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=False, blank=False)
-    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
-    autor = models.ManyToManyField(Autor, related_name="livros")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name="livros")
     capa = models.ForeignKey(
         Image,
         related_name="+",
