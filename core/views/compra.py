39a846ead889f21777db547880351f741d33bcba
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-09-16 11:44:47 -0300

    Filtrando apenas as compras do usuário autenticado

--- core/views/compra.py
+++ core/views/compra.py
@@ -12,3 +12,14 @@ class CompraViewSet(ModelViewSet):
         if self.action in ("create", "update"):
             return CriarEditarCompraSerializer
         return CompraSerializer
+
+class CompraViewSet(ModelViewSet):
+    queryset = Compra.objects.all()
+
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)
