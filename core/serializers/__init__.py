c7d7e88f44fa0a911ea08a7dc0dd9ac43ff84a19
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-09-16 11:15:54 -0300

    Criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,6 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
+from .compra import CompraSerializer, CriarEditarCompraSerializer, ItensCompraSerializer
 from .editora import EditoraSerializer
 from .livro import LivroDetailSerializer, LivroListSerializer, LivroSerializer
 from .user import UserSerializer
-from .compra import CompraSerializer
